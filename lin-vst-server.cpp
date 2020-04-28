8970efcc2e47b0667e1eda28ad9c9a0b85820c35
osxmidi <chopinbig@tutanota.com>
2020-04-28 15:05:41 +1000

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -314,6 +314,7 @@ VstIntPtr rv = 0;
     int handle;
     int width;
     int height;
+    int winerror;    
     } winm2;    
     winmessage *winm;
 #endif
@@ -991,6 +992,30 @@ void RemoteVSTServer::effDoVoid(int opcode)
          return;
     }
 #ifdef EMBED
+   if (opcode == effEditGetRect)
+   {
+    rect = 0;
+    m_plugin->dispatcher(m_plugin, effEditGetRect, 0, 0, &rect, 0);
+//    m_plugin->dispatcher(m_plugin, effEditOpen, 0, 0, hWnd, 0);
+ //   m_plugin->dispatcher(m_plugin, effEditGetRect, 0, 0, &rect, 0);
+ //   m_plugin->dispatcher(m_plugin, effEditClose, 0, 0, 0, 0);
+    if (!rect)
+    {
+        winm->width = 0;
+        winm->height = 0;
+	winm->winerror = 1;    
+        tryWrite(&m_shm[FIXED_SHM_SIZE], winm, sizeof(winmessage));
+        return;
+    }
+    else
+    {
+        winm->width = rect->right - rect->left;
+        winm->height = rect->bottom - rect->top;
+	winm->winerror = 0; 
+        tryWrite(&m_shm[FIXED_SHM_SIZE], winm, sizeof(winmessage));
+        return;
+     }
+    }		
 #ifdef TRACKTIONWM  
     if (opcode == 67584930)
     {
@@ -2623,6 +2648,7 @@ DWORD dwElapsed;
     if (!remoteVSTServerInstance2[pidx]->rect)
     {
     printf("RectErr\n"); 
+    remoteVSTServerInstance2[pidx]->winm->winerror = 1; 	    
     sched_yield();	     
     SetEvent(remoteVSTServerInstance2[pidx]->ghWriteEvent);    
     break;     
