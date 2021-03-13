6c57060dc310a36c64463e43587814799652af01
osxmidi <chopinbig@tutanota.com>
2021-03-13 15:41:53 +1100

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -2479,15 +2479,15 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdlinexxx, int c
     cerr << "Copyright (c) 2004-2006 Chris Cannam" << endl;
     #ifdef EMBED
     #ifdef VST32SERVER
-    cerr << "LinVst3-X version 2.1.5-32bit" << endl;
+    cerr << "LinVst3-X version 3.3-32bit" << endl;
     #else
-    cerr << "LinVst3-X version 2.1.5-64bit" << endl;    
+    cerr << "LinVst3-X version 3.3-64bit" << endl;    
     #endif
     #else
     #ifdef VST32SERVER
-    cerr << "LinVst3-X version 2.1.5st-32bit" << endl;
+    cerr << "LinVst3-X version 3.3st-32bit" << endl;
     #else
-    cerr << "LinVst3-X version 2.1.5st-64bit" << endl;    
+    cerr << "LinVst3-X version 3.3st-64bit" << endl;    
     #endif    
     #endif
     
